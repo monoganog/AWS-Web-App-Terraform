5ef239ec9c8a46b009ba3e31e080bf04f2cc851d
Ben Monaghan <monoganog@gmail.com>
2024-03-26 23:19:06 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -195,7 +195,6 @@ resource "aws_route53_record" "root" {
 
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
-  db_name              = "mydb"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
