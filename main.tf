1a5653a429de12d85615d525a88feac2d3bb2279
Ben Monaghan <monoganog@gmail.com>
2024-04-04 07:48:59 +0100

    Changes to instance 2

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "instance_2" {
   security_groups = [aws_security_group.instances.name]
   user_data       = <<-EOF
               #!/bin/bash
-              echo "Hello, World 2222345" > index.html
+              echo "Hello, World 22223453" > index.html
               python3 -m http.server 8080 &
               EOF
 }
